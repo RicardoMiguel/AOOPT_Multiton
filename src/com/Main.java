4dd14cd04dd9d4386482b83c6cc2a05aeb8f299c
Ricardo Soares <1111114@isep.ipp.pt>
2016-07-23 13:38:37 +0100

    Test commit.

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,7 +3,7 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-        for(int  i = 0; i< 10 ; i++){
+        for(int  i = 0; i< 12 ; i++){
             System.out.println(Database.getConnection());
         }
     }
